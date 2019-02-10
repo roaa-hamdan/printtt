fccfe3d5b80cca06718e416d3222027de6650734
laptop center <laptop center@192.168.1.7>
2019-02-10 21:41:42 +0200

    rr

---
+++ src/printt/rj.java
@@ -0,0 +1,13 @@
+package printt;
+
+import javax.swing.JOptionPane;
+
+public class rj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "some required fields are empty ,please fill them ");
+
+	}
+
+}
